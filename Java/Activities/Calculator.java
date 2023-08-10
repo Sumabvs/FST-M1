bac183f28843d8854e71da74911b9c945fc0446d
Sumabvs <141907487+Sumabvs@users.noreply.github.com>
2023-08-10 13:13:04 +0530

    Add files via upload

---
+++ Java/Activities/Calculator.java
@@ -0,0 +1,18 @@
+package Examples;
+
+public class Calculator {
+
+
+        public int multiply(int a, int b) {
+            return a * b;
+        }
+
+        public int add(int a, int b) {
+            return a + b;
+        }
+    public int square(int num) {
+        return num * num;
+    }
+
+
+}
